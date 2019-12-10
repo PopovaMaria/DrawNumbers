b395f1ef85985b1e5e4dc0f2871f639e1d3b7f29
PopovaMaria <Geer78@ya.ru>
2019-12-11 00:18:01 +0300

    second commit

--- bigdigits.py
+++ bigdigits.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 
 string = []
@@ -39,17 +39,17 @@ Seven = ["*****", " *", " * ", " * ", " * ", "* ", "* "]
 
 Eight = [" *** ", "* *", "* *", " *** ", "* *", "* *", " *** "]
 
-
 Nine = [" ****", "* *", "* *", " ****", " *", " *", " *"]
 
 Digits = [Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
 
 p=sys.argv[1]
 numbers = [int(char) for char in p]
+
 for line in range (7):
     for number in numbers:
                 temp = Digits[number]
-                string.append(temp[line])
+                string.append(temp[line].replace('*', str(number)))
     print(' '.join(string))
     string[:] = []
         
